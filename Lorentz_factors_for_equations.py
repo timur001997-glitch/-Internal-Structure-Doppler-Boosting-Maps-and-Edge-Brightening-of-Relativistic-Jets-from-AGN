f4372f007e32d07a284db1b3e999336097f2c98a
timur001997-glitch <timur001997@gmail.com>
2026-08-27 10:56:13 +0300

    Update Lorentz_factors_for_equations.py

--- Lorentz_factors_for_equations.py
+++ Lorentz_factors_for_equations.py
@@ -3,39 +3,39 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import time
 
-start_time = time.time()  # время начала выполнения
+start_time = time.time()  # start time of calculations
 
-sigma_M = 100
-Gamma_in = 2 #гидродинамический лоренц-фактор на оси джета
-h_0 = 1*10**(1) #магнитное поле на расстоянии одного R_L от центральной машины в Гс
-c = 3*10**10 #скорость света в см/c
-G = 6.67*10**(-8) #гравитационная постоянная в системе СГС
-M = 1*10**5 * 2*10**33 #масса компактного объекта в г
-#M = 2*10**33 #масса Солнца в г
-r_g = 2*G*M/c**2 #гравитационный радиус в см
-R_L = 10*r_g #радиус светового цилиндра в см
+sigma_M = 100 #Michel magnetization parameter
+Gamma_in = 2 #hydrodynamic Lorentz factor on the jet axis
+h_0 = 1*10**(1) #magnetic field at the distance R_L from the central engine in G
+c = 3*10**10 #the speed of light in cm/s
+G = 6.67*10**(-8) #the gravitational constant in the CGS system
+M = 1*10**5 * 2*10**33 #the mass of the compact object in g
+#M = 2*10**33 #the mass of the Sun in g
+r_g = 2*G*M/c**2 #gravitational radius in cm
+R_L = 10*r_g #the radius of the light cylinder in cm
 R_L = 10**(9)
-m_e = 9.1*10**(-28) #масса электрона в г
-q = 4.8 * 10**(-10) #заряд электрона в системе СГС
-t_0 = R_L/c #обезразмеривание времени
+m_e = 9.1*10**(-28) #the mass of an electron in g
+q = 4.8 * 10**(-10) #the electron charge in the CGS system
+t_0 = R_L/c #dimensionless time
 
-P_0 = 4.5 * 10**(-10) #давление в дин/см^2 для M87
-z_00 = 220 * 3.086 * 10**18 #характерное расстояние вдоль оси джета в см для M87
-z_00 = z_00/10**16 #характерное безразмерное расстояние вдоль оси джета для M87
-Psi_0 = np.pi #полный магнитный поток!
-r_jet_cr = np.sqrt(Gamma_in*sigma_M) #в световых цилиндрах R_L (безразмерная)
-r_core = 1 #в световых цилиндрах R_L (безразмерная)
+P_0 = 4.5 * 10**(-10) #pressure in dyn/cm^2 for M87
+z_00 = 220 * 3.086 * 10**18 #the characteristic distance along the jet axis in cm for M87
+z_00 = z_00/10**16 #the characteristic dimensionless distance along the jet axis in cm for M87
+Psi_0 = np.pi #full magnetic flux!
+r_jet_cr = np.sqrt(Gamma_in*sigma_M) #in light cylinders R_L (dimensionless)
+r_core = 1 #in light cylinders R_L (dimensionless)
 
-delta = 10**(-4) #анти-поломка программы - малая добавка delta
+delta = 10**(-4) #anti-breakage program - small delta supplement
 
 #epsilon = 1/sigma_M**2
 Omega_0 = 1
 
 print(r_jet_cr)
 
-#Система двух обыкновенных диффур на M2 и Y, где
-#M2 - квадрат альфвеновского числа Маха, Y - обезразмеренный магнитный поток
-#Система для новых интегралов движения Omega(Psi) и L(Psi)
+#The system of two ordinary differential equations for M2 and Y, where
+#M2 - the square of the Alfven Mach number, Y - dimensionless magnetic flux
+#The system for new integrals of motion Omega(Psi) and L(Psi)
 def f(X, x_r):
    Y = X[0]
    M2 = X[1]
@@ -44,12 +44,12 @@ def f(X, x_r):
    diff_2 = 1/(Gamma_in**2 + x_r**2*(1 - Y/sigma_M) - 1) * (2*x_r*M2*(1 - Y/sigma_M) - Gamma_in**2*x_r*M2*(1 - Y/sigma_M)/A + 4*Y**2*M2**3*(1 - Y/sigma_M)/x_r**3/A - 1/sigma_M*x_r**2*M2*diff_1)
    return [diff_1, diff_2]
 
-#Лоренц-фактор из уравнения Грэда-Шафранова
+#The Lorentz factor from the Grad-Shafranov equation
 def Gamma_GS(x_r, M2, Y):
     Gamma_GS = (Gamma_in - M2*(Gamma_in + 2*Y*(1-Y/sigma_M)))/(1 - x_r**2*(1-Y/sigma_M) - M2)
     return Gamma_GS
 
-#Максимальный лоренц-фактор
+#The maximum Lorentz factor
 def Gamma_max(Y):
     Gamma_max = Gamma_in + 2*Y * (1 - Y/sigma_M)
     return Gamma_max
@@ -58,7 +58,7 @@ def epsilon(Y):
     epsilon_func = 1/2/Gamma_max(Y)**2
     return epsilon_func
 
-#Гидродинамический лоренц-фактор через МГД
+#Hydrodynamic Lorentz factor in the framework of MHD
 def Gamma_dr_MHD(B_p,B_phi,E):
     B = np.sqrt(B_p**2 + B_phi**2)
     beta_dr = E/B
@@ -66,14 +66,14 @@ def Gamma_dr_MHD(B_p,B_phi,E):
     Gamma_dr = np.sqrt(Gamma_in**2 - 1 + Gamma_dr**2)
     return Gamma_dr
 
-#Гидродинамический лоренц-фактор с учётом epsilon
+#Hydrodynamic Lorentz factor taking into account epsilon
 def Gamma_dr_epsilon(x_r, Y):
     beta_dr = x_r*np.sqrt(1 - Y/sigma_M)/np.sqrt(1 + (1+epsilon(Y))**2 * x_r**2*(1 - Y/sigma_M))
     Gamma_dr = 1/np.sqrt(1 - beta_dr**2)
     Gamma_dr = np.sqrt(Gamma_in**2 - 1 + Gamma_dr**2)
     return Gamma_dr
 
-#Гидродинамический лоренц-фактор в случае Omega=const
+#Hydrodynamic Lorentz factor in the case of Omega=const
 def Gamma_dr_linear(x_r):
     Gamma_dr = np.sqrt(1 + x_r**2)
     Gamma_dr = np.sqrt(Gamma_in**2 - 1 + Gamma_dr**2)
@@ -91,7 +91,7 @@ def Omega_F(Y):
     Omega_F = Omega_0 * np.sqrt(1 - Y/sigma_M)
     return Omega_F
 
-#Построение профиля локальной замагниченности
+#Building the local magnetization profile
 def L(Y):
     L = Omega_0 * Psi(Y)/(4*np.pi**2) * np.sqrt(1 - Psi(Y)/Psi_0)
     return L
@@ -105,16 +105,16 @@ def sigma(x_r,Y,M2):
     return sigma
 
 
-N = 100000 #Число пространственных точек определения лоренц-фактора
+N = 100000 #The number of spatial points for determining the Lorentz factor
 
-#Введём начальные условияx
+#Introduction of the initial conditions
 rho_0 = 0.001
 #M2_0 = 3
 M2_0 = 3
 Y_0 = 1/2*Gamma_in/M2_0*rho_0**2
 ics=[Y_0, M2_0]
-#rho = np.linspace(rho_0,53.9,N) #расстояние от оси джета в R_L
-rho = np.linspace(rho_0,53.9,N) #расстояние от оси джета в R_L
+#rho = np.linspace(rho_0,53.9,N) #distance from the jet axis in R_L
+rho = np.linspace(rho_0,53.9,N) #distance from the jet axis in R_L
 
 solution = odeint(f, ics, rho)
 Y = solution[:,0]
@@ -124,13 +124,13 @@ print(M2)
 
 print(epsilon(Y))
 
-#Вычислим значения электромагнитных полей
+#Calculate the values of electromagnetic fields
 Psii = Psi(Y)
 Omega_FF = Omega_F(Y)
 II = I(rho, Y, M2)
 K = len(Y)
 rho_MHD = np.zeros(K-1)
-#Конфигурация электромагнтиных полей через вектора
+#Configuration of electromagnetic fields through vectors
 B_p = np.zeros(K-1)
 E = np.zeros(K-1)
 B_phi = np.zeros(K-1)
@@ -196,7 +196,7 @@ plt.grid(True)
 plt.legend(fontsize=14)
 plt.show()
 
-end_time = time.time()  # время окончания выполнения
-execution_time = end_time - start_time  # вычисляем время выполнения
+end_time = time.time()  # the end time of the calculation
+execution_time = end_time - start_time  # calculating the program runtime.
 
-print(f"Время выполнения программы: {execution_time} секунд")
+print(f"Program execution time: {execution_time} seconds")
